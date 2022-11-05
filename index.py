5720a05dd1af23e3d103032c6b6875071bfb8e8a
Daniel Broomhead <lilp1xel1@gmail.com>
2022-11-05 16:03:08 +1100

    Project Final Commit

--- index.py
+++ index.py
@@ -1,12 +1,13 @@
-from pedalboard import Pedalboard, Chorus, Reverb, Compressor, Gain, LadderFilter, Phaser, Delay, Distortion, PitchShift, Mix
+from pedalboard import Pedalboard, Chorus, Reverb, Compressor, Gain, LadderFilter, Phaser, Delay, PitchShift
 from pedalboard.io import AudioFile
 with AudioFile('sound.mp3') as f:
   audio = f.read(f.frames)
   samplerate = f.samplerate
+print('\n')
 board = Pedalboard([
 ])
-
-effect1 = input('What effects would you like? ').lower()
+print('Chorus, Reverb, Compressor, Gain, HPF, Phaser, Delay, Pitchshift 6ST, PitchShift 12ST, PitchShift Minus 6ST, PitchShift Minus 12ST \n')
+whatEffect = input('What effects would you like? ').lower()
 
 def chorus():
   board.append(Chorus())
@@ -27,10 +28,8 @@ def phaser():
   board.append(Phaser())
 
 def delay():
-  board.append(Delay(delay_seconds=0.25, mix=1.0))
+  board.append(Delay(delay_seconds=0.1, mix=1.0))
 
-def distortion():
-  board.append(Distortion)
 
 def pitchshift6ST():
   board.append(PitchShift(semitones=6))
@@ -44,6 +43,34 @@ def pitchshiftMinus6ST():
 def pitchshiftMinus12ST():
   board.append(PitchShift(semitones=-12))
 
+e = whatEffect.split(", ")
+
+effects = {
+  "chorus" : chorus,
+  "reverb" : reverb,
+  "compressor" : compressor,
+  "gain" : gain,
+  "hpf" : ladderfilter,
+  "phaser" : phaser,
+  "delay" : delay,
+  "pitchshift 6st" : pitchshift6ST,
+  "pitchshift 12st" : pitchshift12ST,
+  "pitchshift minus 6st" : pitchshiftMinus6ST,
+  "pitchshift minus 12st" : pitchshiftMinus12ST,
+}
+
+
+for x in e:
+    try:
+        plugin = effects[x]
+    except KeyError:
+        print(f'"{x}" is not in the effect list, are you sure you spelt it correctly?')
+        continue # Skip to next one; prevent code below from running
+
+    plugin()
+
+
 effected = board(audio, samplerate)
 with AudioFile('processed-output.wav', 'w', samplerate, effected.shape[0]) as f:
   f.write(effected)
+  print('Audio successfully effected, you can find it at "processed-output.wav"')
